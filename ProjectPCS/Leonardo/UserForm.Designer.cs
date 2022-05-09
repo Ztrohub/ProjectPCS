99cbe57e1d350423b3060bdada601429c504b07a
Leozer HD <leonardminoto@gmail.com>
2022-05-09 11:19:29 +0700

    tambah button

--- ProjectPCS/Leonardo/UserForm.Designer.cs
+++ ProjectPCS/Leonardo/UserForm.Designer.cs
@@ -29,13 +29,25 @@ namespace ProjectPCS.Leonardo
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // UserForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "UserForm";
             this.Text = "UserForm";
             this.Load += new System.EventHandler(this.UserForm_Load);
@@ -44,5 +56,7 @@ namespace ProjectPCS.Leonardo
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
